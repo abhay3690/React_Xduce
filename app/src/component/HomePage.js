b8813a67eae91557e16cb5328f5e5e37407634a1
abhay3690 <abhay7777@gmail.com>
2024-05-23 18:02:43 +0530

    Add fullstack axios,backend connect

--- app/src/component/HomePage.js
+++ app/src/component/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-class HomePage extends React.Component {
+class HomePage  {
+  fethch
   render() {
     return (
       <div>
         <h1>Home Page</h1>
         <p>Welcome to the home page!</p>
       </div>
+   
     );
   }
 }
