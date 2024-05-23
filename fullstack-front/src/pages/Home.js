cb5d3e4797dc125fd30522ef31f20be59127019a
abhay3690 <abhay7777@gmail.com>
2024-05-23 22:08:36 +0530

    edit and view

--- fullstack-front/src/pages/Home.js
+++ fullstack-front/src/pages/Home.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Link, useParams } from "react-router-dom";
 export default function Home() {
   const [users, setUsers] = useState([]);
+  const {id} = useParams()
   useEffect(() => {
     loadUsers();
   }, []);
@@ -9,6 +11,11 @@ export default function Home() {
     const result = await axios.get("http://localhost:8080/user");
     setUsers(result.data);
   };
+
+  const deleteUser=async(id)=>{
+    await axios.delete(`http://localhost:8080/user/${id}`)
+    loadUsers()
+  }
   return (
     <div className="container">
       <div className="py-4">
@@ -30,6 +37,11 @@ export default function Home() {
              <td>{user.name}</td>
              <td>{user.username}</td>
              <td>{user.email}</td>
+             <td>
+              <Link className="btn btn-primary mx-2" to={`/viewuser/${user.id}`}>view</Link>
+              <Link className="btn btn-outline-primary mx-2" to={`/edituser/${user.id}`}>Edit</Link>
+              <button className="btn btn-danger mx-2" onClick={()=>deleteUser(user.id)}>Delete</button>
+             </td>
            </tr>
             ))}
            
