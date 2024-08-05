b364b2f5c6d45bb2849c4b59b335e2e3a352cede
abhay3690 <abhay7777@gmail.com>
2024-08-05 19:54:17 +0530

    connection with backend

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,4 +1,5 @@
-import { useEffect } from "react";
+import { useState } from "react";
+import UserList from './components/UserList'; 
 
 import "./App.css";
 
@@ -6,8 +7,16 @@ import "./App.css";
 
 
 function App() {
+  const [searchQuery , setSearchQuery] = useState();
+  const [searchResults , setSearchResults] = useState([]);
+
+  const searchHandler = (event) => {
+     console.log(event.target.value , "this is the value for the search");
+  }
   return (
-   <div></div>
+   <div className="app">
+    <UserList></UserList>
+  </div>
     
   );
 }
---
+++ myapp/src/users.js
@@ -0,0 +1,39 @@
+export const users = [
+    {
+      "ID": 1,
+      "FirstName": "Jane",
+      "LastName": "Doe",
+      "Email": "jane.doe@example.com",
+      "Gender": "Female"
+    },
+    {
+      "ID": 2,
+      "FirstName": "John",
+      "LastName": "Smith",
+      "Email": "john.smith@example.com",
+      "Gender": "Male"
+    },
+    {
+      "ID": 3,
+      "FirstName": "Alex",
+      "LastName": "Johnson",
+      "Email": "alex.johnson@example.com",
+      "Gender": "Non-binary"
+    },
+    {
+      "ID": 4,
+      "FirstName": "Emily",
+      "LastName": "Williams",
+      "Email": "emily.williams@example.com",
+      "Gender": "Female"
+    },
+    // ... Add more entries here up to 1000
+    {
+      "ID": 1000,
+      "FirstName": "Chris",
+      "LastName": "Martin",
+      "Email": "chris.martin@example.com",
+      "Gender": "Male"
+    }
+  ]
+  
