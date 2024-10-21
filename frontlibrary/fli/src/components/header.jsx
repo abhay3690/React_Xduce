48b1132feec480457d1b9f986f4b35acb44f2854
Abhay S <abhaysuthar7777@gmail.com>
2024-10-21 22:06:32 +0530

    add

--- frontlibrary/fli/src/components/header.jsx
+++ frontlibrary/fli/src/components/header.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const header = () => {
   return (
     <div>
       <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdownMenuLink"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Dropdown link
-              </a>
+        <a
+          className="nav-link dropdown-toggle"
+          href="#"
+          id="navbarDropdownMenuLink"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          Dropdown link
+        </a>
+      </li>
     </div>
-  )
-}
+  );
+};
 
-export default header
+export default header;
